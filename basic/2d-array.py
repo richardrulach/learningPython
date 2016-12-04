33fccda1441e664366892e4c7ab35ef5bce60150
Richard <richardrulach@users.noreply.github.com>
2016-12-04 11:43:43 +0000

    Create 2d-array.py

---
+++ basic/2d-array.py
@@ -0,0 +1,38 @@
+#####################################################
+# Author:       RR
+# Created:      04/12/2016
+#
+# Description: DEMONSTRATES 2D ARRAY IMPLEMENTED
+#               AS PYTHON LISTS
+#####################################################
+#
+#
+#
+#
+#####################################################
+
+print("Array demonstration:\n")
+
+myarray = [
+    ["peter","paul","david","mike","steve"],
+    ["sally","anne","tracy","emma","mary"]
+    ]
+
+print("Number of elements in myarray: " + str(len(myarray)))
+print("Number of elements in myarray[0]: " + str(len(myarray[0])))
+print("Number of elements in myarray[1]: " + str(len(myarray[1])))
+print("\n")
+
+print("Here is myarray:")
+print(myarray)
+print("\n")
+
+print("Here is myarray[0]:")
+print(myarray[0])
+print("\n")
+
+print("Here is myarray[1]:")
+print(myarray[1])
+print("\n")
+      
+
