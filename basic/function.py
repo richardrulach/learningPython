0d5e785ee557aa868be8e2e71f0c585c928d23f0
Richard <richardrulach@gmail.com>
2016-12-04 21:15:20 +0000

    Initialised

---
+++ basic/function.py
@@ -0,0 +1,23 @@
+#####################################################
+#   Author:       RR
+#   Created:      04/12/2016
+#
+#   Description:  DEMONSTRATES 2D ARRAY IMPLEMENTED
+#                 AS PYTHON LISTS
+#####################################################
+#
+#   DATE        AUTHOR  DESCRIPTION
+#   04/12/2016  RR      initialised
+#
+#####################################################
+
+def AskQuestion(question):
+    print(question)
+    val = input()
+    print("Your answer was:" + val)
+
+print("Start of program")
+AskQuestion("What is your name?")
+AskQuestion("What is your favourite colour?")
+
+print("End of program")
