86d18e6b2a3ceb5672f337dcc609f74b33686d4d
Richard <richardrulach@gmail.com>
2016-12-04 21:53:11 +0000

    initialised

---
+++ basic/dictionary.py
@@ -0,0 +1,34 @@
+#####################################################
+#   Author:       RR
+#   Created:      04/12/2016
+#
+#   Description:  STORING 
+#####################################################
+#
+#   DATE        AUTHOR  DESCRIPTION
+#   04/12/2016  RR      initialised
+#
+#####################################################
+
+print("Dictionary demonstration:\n")
+
+mydictionary = {
+    "boys": ["peter","paul","david","mike","steve"],
+    "girls":["sally","anne","tracy","emma","mary"]
+    }
+
+print(len(mydictionary))
+
+print("boys:")
+print(mydictionary["boys"])
+
+print("girls:")
+print(mydictionary["girls"])
+
+print("boys - 2")
+print(mydictionary["boys"][2])
+
+print("girls - 3")
+print(mydictionary["girls"][3])
+
+
