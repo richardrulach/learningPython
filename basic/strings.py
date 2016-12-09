8e444019c27e24dd5c213effc93ffccadde1036c
Richard <richardrulach@gmail.com>
2016-12-09 08:21:22 +0000

    initialised

---
+++ basic/strings.py
@@ -0,0 +1,23 @@
+#####################################################
+#   Author:       RR
+#   Created:      04/12/2016
+#
+#   Description:  Working with strings
+#####################################################
+#
+#   DATE        AUTHOR  DESCRIPTION
+#   09/12/2016  RR      initialised
+#
+#####################################################
+
+s = "Where is there hope in the world?"
+
+print(s[0:5])
+
+print(s[-6:])
+
+print(s[::-1])
+
+print(s[::2])
+
+
