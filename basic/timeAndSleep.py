55769b16053516bba0e69ed7e2f89adca69cf25d
Richard <richardrulach@gmail.com>
2016-12-12 00:20:00 +0000

    tidied sleep statements

--- basic/timeAndSleep.py
+++ basic/timeAndSleep.py
@@ -12,8 +12,7 @@
 import time
 import sys
 
-
-def printy(x):
+def pr(x):
      sys.stdout.write(x)
 
 print(1)
@@ -28,10 +27,10 @@ time.sleep(0.3)
 print(4)
 
 print('What size of tree do you want?\n')
-size = int(input())
+size = int(input()) + 1
 
 for x in range(1,size):
-     printy(' ' * (size - x))
-     printy('x' * (x * 2 - 1) + '\n')
-     time.sleep(0.1)
+     pr(' ' * (size - x + 2))
+     pr('x' * (x * 2 - 1) + '\n')
+     time.sleep(0.2)
 
